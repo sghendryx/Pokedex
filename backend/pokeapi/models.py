165ce36d8362ae5c5e5b2639411a0837b0585cce
Ian Merrill <ian.merrill9@gmail.com>
2022-11-19 12:55:17 -0700

    areas

--- backend/pokeapi/models.py
+++ backend/pokeapi/models.py
@@ -10,3 +10,9 @@ class Location(models.Model):
     name = models.CharField(max_length=50)
     poke_id = models.IntegerField()
     region = models.ForeignKey(Region, on_delete=models.CASCADE)
+
+
+class Area(models.Model):
+    name = models.CharField(max_length=50)
+    poke_id = models.IntegerField()
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
