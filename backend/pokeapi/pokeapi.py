def2f217ec88aa1ad9eacf4fe0319edb482e0a7a
sghendryx <sghendryx@gmail.com>
2022-11-20 11:18:55 -0700

    Backend Pokemon Info
    Querying the API for the pokemon and information we're looking for encountered in a specific area

--- backend/pokeapi/pokeapi.py
+++ backend/pokeapi/pokeapi.py
@@ -4,19 +4,34 @@ from pokeapi.models import Region, Location, Area
 
 # This is where we will place all the logic to retrieve the data we need from the pokemon API and format it to a form
 # that will be useful to see in the UI.
+
+#Adds Pokemon you encounter's URLs in a location_area to the empty array pokemon_urls 
+def pokemon_route_encounters():
+    response = requests.get("https://pokeapi.co/api/v2/location-area/kanto-route-28-area/")
+    area = response.json()
+    encounters = area['pokemon_encounters']
+    pokemon_names = []
+    for e in encounters:
+        pokemon = e['pokemon']
+        pokemon_names.append(pokemon['name'])
+
+    return pokemon_names
+
+
+#Gets a pokemon's name and its sprite from the pokemons in the specified location area.
 def get_pokemon():
-    response = requests.get("https://pokeapi.co/api/v2/pokemon/?limit=150")
-    json = response.json()
-    pokemon = json['results']
-    hydrated_pokemon = []
-    for p in pokemon:
-        hydration_res = requests.get(p['url'])
-        hydration_json = hydration_res.json()
+    pokemon_names = pokemon_route_encounters()
+    hydrated_pokemon = [] 
+    for name in pokemon_names:
+        response = requests.get(f'https://pokeapi.co/api/v2/pokemon/{name}')
+        hydration_json = response.json()
         sprite = hydration_json['sprites']['front_default']
-        hydrated_pokemon.append({'name': p['name'], 'sprite': sprite})
+        type = hydration_json['types']
+        hydrated_pokemon.append({'name': name, 'sprite': sprite, 'type': type})
 
     return {'pokemon': hydrated_pokemon}
 
 
 def test():
-    print("testing ground")
+    return get_pokemon()
+    
